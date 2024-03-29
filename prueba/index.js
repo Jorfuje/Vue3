39fdbc87c5163459f50c025c8af0e66c61d4a41d
Jorfuje <arbjorfuje@gmail.com>
2024-03-28 21:43:30 -0600

    Empezando con TypeScript

---
+++ prueba/index.js
@@ -0,0 +1,9 @@
+var variableNumerica;
+var variableString;
+var variableBooleana;
+variableNumerica = 1;
+variableString = "Hola desde TypeScript";
+variableBooleana = true;
+console.log(typeof variableNumerica);
+console.log(typeof variableString);
+console.log(typeof variableBooleana);
---
+++ prueba/index.ts
@@ -0,0 +1,14 @@
+let variableNumerica: number;
+let variableString: string;
+let variableBooleana: boolean;
+
+variableNumerica = 1;
+variableString = "Hola desde TypeScript";
+variableBooleana = true;
+
+console.log(typeof variableNumerica);
+console.log(typeof variableString);
+console.log(typeof variableBooleana);
+
+
+variableNumerica
